60ef8a9114e43f47343048a88889db7ffd583425
Gabriela <gabirmsoares@gmail.com>
2026-03-24 20:23:50 -0300

    CREATE: seed.ts  RBAC

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config'
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
+
+
+async function createPermission() {
+  await prisma.permission.create({
+    data: {
+      key: "1",
+      description: "view.product",
+    },
+  })
+}
+
+async function createrole() {
+  await prisma.role.create({
+    data: {
+      name: 'Atendente',
+      description: "Atendente da bomboniere",
+    },
+  })
+}
+
+async function createUser(){
+  await prisma.user.create({
+    data: {
+      name: 'Gabriela',
+      email: 'gabriela@email.com',
+    },
+  })
+}
+
+async function main() {
+  await createPermission()
+  await createrole()
+  await createUser()
+}
+
+main().finally(() => {
+  prisma.$disconnect()
+})
